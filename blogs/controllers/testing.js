2d8dadeedb7405e6160f70013e8004e7682e07ae
Clare <clareivers@gmail.com>
2024-06-10 15:10:35 +1000

    add nodemon to test run script & add delete blogs to testing database reset script

--- blogs/controllers/testing.js
+++ blogs/controllers/testing.js
@@ -1,14 +1,11 @@
 const testingRouter = require('express').Router()
-const Note = require('../models/blogs')
+const Blog = require('../models/blogs')
 const User = require('../models/users')
 
 testingRouter.post('/reset', async (request, response) => {
   console.log('resetting db....')
-  // await Blog.deleteMany({})
+  await Blog.deleteMany({})
   await User.deleteMany({})
-
-  // const users = User.find({})
-  // console.log('users are ',users.length)
 
   response.status(204).end()
 })
